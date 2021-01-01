42d69475a18d0a1721d9f73a745d69e7bf37bda3
pfotis <69961508+pfotis@users.noreply.github.com>
2021-01-01 11:37:44 +1100

    create function to get data from API

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,19 @@
-var inputElement = $("#city-input").val().trim();
+
+$("#search-button").on("click", function(event) {
+    event.preventDefault();
+    
+    
+    getDataOfWeather();
+});
+
+function getDataOfWeather(){
+    var inputElement = $("#city-input").val().trim();
+    console.log(inputElement);
+    var  queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputElement + "&appid=65288d77b29b169fdd8cf60a7f46c61d";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+    console.log(response);
+    });
+}

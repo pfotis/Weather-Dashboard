bc46419127d4b9a12e3d8c9c4f0eee6c3fc9af56
pfotis <69961508+pfotis@users.noreply.github.com>
2021-01-03 14:01:22 +1100

    almost finish with the today weather

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -30,14 +30,64 @@ function getDataOfWeather(place){
         method: "GET"
     }).then(function(response) {
         var forcastData = response;
-        console.log(forcastData);
         var  uviURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + forcastData.city.coord.lat + "&lon=" + forcastData.city.coord.lon + "&exclude=hourly,daily&appid=" + key;
         $.ajax({
             url: uviURL,
             method: "GET"
         }).then(function(response) {
-        var uviData = response;
-        console.log(uviData);
+            var uviData = response;
+
+            console.log(forcastData);
+            $("#weatherToday").css("border","1px solid black");
+            var cityH2 = $("<h2>");
+            var date = (forcastData.list[4].dt_txt).split(" ");
+           
+            console.log(date[0]);
+            cityH2.text(forcastData.city.name + " "+ "(" + date[0] + ")");
+            $("#weatherToday").empty().append(cityH2);
+            //var image = "/assets/images/icons/" + forcastData.list[4].weather[0].icon + ".png";               
+            space();
+
+            var tempDiv = $("<div>");
+            tempDiv.text("Temperature:"+forcastData.list[4].main.temp+"F");
+            $("#weatherToday").append(tempDiv);
+            space();
+
+            var humDiv = $("<div>");
+            humDiv.text("Humidity:"+forcastData.list[4].main.humidity+"%");
+            $("#weatherToday").append(humDiv);
+            space();
+
+            var windDiv = $("<div>");
+            windDiv.text("Wind Speed:"+forcastData.list[4].wind.speed+" MPH");
+            $("#weatherToday").append(windDiv);
+            space();
+
+
+
+            var uviLabel = $("<div>");
+            uviLabel.addClass("label");
+            uviLabel.text("UV Index:");
+            $("#weatherToday").append(uviLabel);
+            var uviDiv = $("<div>");
+            if(uviData.current.uvi < 2){
+                uviDiv.addClass("green");
+            }
+            else if(uviData.current.uvi>2 && uviData.current.uvi < 5){
+                uviDiv.addClass("yellow");
+            }
+            else if(uviData.current.uvi>5 && uviData.current.uvi < 7){
+                uviDiv.addClass("orange");
+            }
+            else if(uviData.current.uvi>7 && uviData.current.uvi < 10){
+                uviDiv.addClass("red");
+            }else if(uviData.current.uvi>10){
+                uviDiv.addClass("purple");
+            }
+            
+            uviDiv.text(uviData.current.uvi);
+           
+            $("#weatherToday").append(uviDiv);
         });
     });
     
@@ -52,4 +102,13 @@ function listOfPlaces(city) {
     buttonCity.text(city);
     $("#listOfCities").append(divCity);
     divCity.append(buttonCity);
+}
+
+function space(){
+    var spaceBr = $("<br>");
+    $("#weatherToday").append(spaceBr);
+}
+
+function attachImage(icon){
+    
 }
